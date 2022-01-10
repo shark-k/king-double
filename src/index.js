52e6cf68c94be7054c28174b6fa87981a1fd0a63
Administrator <fengyued223@163.com>
2022-01-10 23:01:34 +0800

    案例完善

--- src/index.js
+++ src/index.js
@@ -1,13 +1,25 @@
 const  express=require("express");
+const path=require("path");
 const homeRouter=require("./routers/homeRouter");
-
+const {notFoundMF, rizhiMF, handleErrorMF,crossDomainM}=require("./middleware/sz_middleware");
+const url = require("url");
 //创建服务程序
 let app=express();
-
-//挂载路由中间件
+//跨域中间件
+app.use(crossDomainM);
+//处理post请求中间件
+app.use(express.json(),express.urlencoded({extended:true}));
+// 日志中间件
+app.use(rizhiMF)
+// //静态资源中间件
+app.use(express.static(path.resolve(__dirname,"public")));
+// //挂载路由中间件
 app.use("/",homeRouter);
-abcdd();
 
+//404中间件
+app.use(notFoundMF(path.resolve(__dirname,"./defaultPages/404.html")));
+//500中间件
+app.use(handleErrorMF(path.resolve(__dirname,"./defaultPages/500.html")));
 //创建程序监听
 app.listen(5000,()=>{
     console.log("服务器启动成功！")
--- src/middleware/sz_middleware.js
+++ src/middleware/sz_middleware.js
@@ -1,8 +1,9 @@
 const path=require('path');
-const {getCurrentTime}=require('../tool/sz_dateTime')
+const {getCurrentTime}=require('../tools/sz_dateTime')
 const fs=require('fs');
-const {sendMail}=require('../tool/sz_email');
-
+const {sendMail}=require('../tools/sz_email');
+const {execSQL}=require("../tools/sz_mysql");
+//404中间件
 let notFoundMF=function (notFoundFilePath){
     if(!path.isAbsolute(notFoundFilePath)){
         throw Error('必须传递一个绝对路径');
@@ -11,50 +12,93 @@ let notFoundMF=function (notFoundFilePath){
         resp.status(404).sendFile(notFoundFilePath);
     }
 }
+//日志中间件
+let rizhiMF= (req,resp,next)=>{
+        //日志记录
 
-let rizhiMF=function (rizhiFilePath){
+        // let current=getCurrentTime();
+        let method=req.method;
+        let path=req.path;
+        let params={};
+        if(method.toLowerCase()==='get'){
+            params=req.query;
+        }else if(method.toLowerCase()==='post'){
+            params=req.body;
+        }
+        let ua=req.headers['user-agent'];
+        //写入系统日志
+//         let result=`
+// =================================================
+//     请求时间:${current}
+//     请求方法:${method}
+//     请求路径:${path}
+//     请求参数:${JSON.stringify(message)}
+//     客户端信息:${ua}
+// =================================================
+// `
+//         fs.appendFile(rizhiFilePath,result,()=>{
+//             console.log('日志写入成功！')
+//             //调用邮件中间件
+//             sendMail('594135484@qq.com','启动报告',result);
+//         });
+        //写入系统数据库
+        execSQL("insert into t_log (method,path,params,user_agent) values(?,?,?,?)",[method,path,JSON.stringify(params),ua]).then(result=>{
+            console.log("记录日志成功！");
+        });
+        //继续执行
+        next();
 
-let handlerErrorMF=function (errorFilePath,errorResponseFilePath){
-if(!path.isAbsolute(errorFilePath) || !path.isAbsolute(errorResponseFilePath)){
-    throw Error('请输入一个绝对路径')
 }
-return (err,req,resp,next)=>{
-    let current=getCurrentTime();
-    let method=req.method;
-    let path=req.path;
-    let message={};
-    if(method.toLowerCase()==='get'){
-        message=req.query;
-    }else if(method.toLowerCase()==='post'){
-        message=req.params;
+
+//错误中间件
+let handleErrorMF=function (errorResponseFilePath){
+    if (!path.isAbsolute(errorResponseFilePath)){
+        throw Error("请传入一个绝对路径");
     }
-    let ua=req.headers['user-agent'];
-    let err_type=err.name;
-    let err_msg=err.message;
-    let err_stack=err.stack;
-    let info=`
-    ====================================
-    请求时间:${current}
-    请求方法:${method}
-    请求路径:${path}
-    请求参数:${JSON.stringify(message)}
-    客户端信息:${ua}
+    return  (err,req,resp,next)=>{
+        //错误类型
+        let err_type=err.name;
+        //错误描述
+        let err_msg=err.message;
+        //错误堆栈
+        let err_stack=err.stack;
+
+        let info=`
+    ===============================================
     错误类型:${err_type}
     错误描述:${err_msg}
-    错误事件:${getCurrentTime}
+    错误时间:${getCurrentTime()}
     错误堆栈:${err_stack}
-    =====================================
+    ===============================================
     `
-    fs.appendFile(errorFilePath,info,()=>{
-        sendMail('594135484@qq.com','错误报告',info);
-    });
-    resp.status(500).sendFile(errorResponseFilePath)
+        //1-写入日志 发送邮件
+    //   fs.appendFile(errorFilePath,info,()=>{
+    //   console.log('捕获到错误');
+    //   resp.status(500).sendFile(errorResponseFilePath);
+        //调用邮件中间件
+    //  sendMail('594135484@qq.com','错误报告',info);
+    //     });
+        //2-写入数据库 发送邮件
+        execSQL("insert into t_error (err_type,err_msg,err_stack) values (?,?,?)",[err_type,err_msg,JSON.stringify(err_stack)]).then(result=>{
+            if (result.affectedRows>=1){
+                //调用邮件中间件
+                sendMail('594135484@qq.com','错误报告',info);
+            }
+})
+        resp.status(500).sendFile(errorResponseFilePath);
+    }
 }
+//跨域中间件
+let crossDomainM=(req,resp,next)=>{
+    resp.header("Access-Control-Allow-Origin","*")
+    resp.header("Access-Control-Allow-Methods",'GET,PUT,POST,DELETE')
+    resp.header("Access-Control-Allow-Headers","Content-Type")
+   next()
 }
-
-}
+//回调参数
 module.exports={
     notFoundMF,
     rizhiMF,
-
+    handleErrorMF,
+    crossDomainM
 }
